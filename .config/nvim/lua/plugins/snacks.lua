71124f42084dceb02b6b057090e43a86167f0dfd
Mathis Imming <mathis.imming@sap.com>
2025-02-26 15:15:44 +0100

    fixup snacks

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -17,7 +17,7 @@ return {
     quickfile = { enabled = true },
     scope = { enabled = true },
     scroll = { enabled = false },
-    statuscolumn = { enabled = true },
+    statuscolumn = { enabled = false },
     words = { enabled = true },
     styles = {
       notification = {
