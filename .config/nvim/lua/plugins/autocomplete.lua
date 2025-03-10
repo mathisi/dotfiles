ecee7d088d926b3937aaba568c6ff8a924f978b8
Mathis Imming <mathis.imming@sap.com>
2025-03-10 10:47:26 +0100

    update packages and fix blink

--- .config/nvim/lua/plugins/autocomplete.lua
+++ .config/nvim/lua/plugins/autocomplete.lua
@@ -10,8 +10,8 @@ return {
 
         ['<C-y>'] = { 'select_and_accept', 'fallback' },
 
-        ['<Tab>'] = { 'select_next', 'fallback' },
-        ['<S-Tab>'] = { 'select_prev', 'fallback' },
+        ['<C-n>'] = { 'select_next', 'fallback' },
+        ['<C-p>'] = { 'select_prev', 'fallback' },
 
         ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
         ['<C-x>'] = { 'hide' },
@@ -19,13 +19,20 @@ return {
         ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
         ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
 
-        ['<C-n>'] = { 'snippet_forward', 'fallback' },
-        ['<C-p>'] = { 'snippet_backward', 'fallback' },
+        ['<Tab>'] = { 'snippet_forward', 'fallback' },
+        ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
       },
 
       appearance = {
         use_nvim_cmp_as_default = true,
         nerd_font_variant = 'mono',
+      },
+
+      cmdline = {
+        completion = {
+          ghost_text = { enabled = false },
+        },
+        enabled = true,
       },
 
       sources = {
